4489c2eeb175f028fc8baddc736453c16e2381c4
Sahil Rawat <sahilr8587@gmail.com>
2025-02-07 17:05:37 +0530

    Update Grid component width for improved layout on About page

--- client/src/pages/About/components/Grid.tsx
+++ client/src/pages/About/components/Grid.tsx
@@ -3,7 +3,7 @@ const Grid = () => {
   return (
     
     <section className="flex flex-col items-center justify-center w-full bg-black py-10">
-        <div className="flex justify-center items-center w-[80vw] ">
+        <div className="flex justify-center items-center w-[90vw] ">
         <img src="/images/grid.png" alt="grid" /></div>
     </section>
   )
