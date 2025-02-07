dd0f372aedaa75167586adedea42521006403ba5
Ankit-0009 <ankitcode2511@gmail.com>
2025-02-07 17:01:15 +0530

    Add lead generation functionality; implement Redux store and LeadSlice; include Axios instance; add service images and update ServiceDetail component

--- client/src/pages/Services/components/ServiceDetail.tsx
+++ client/src/pages/Services/components/ServiceDetail.tsx
@@ -28,34 +28,25 @@ const ServiceDetail = () => {
                 <h2 className="text-white text-center md:text-5xl text-2xl font-poppins font-semibold">{service.title}</h2>
             </div>
 
-            <h1 className='font-poppins md:text-2xl text-lg my-16 font-medium text-white md:text-center w-[80vw]'><span className='text-[#FC7901]'>At MarketingEXL,</span> we connect your brand with potential customers through comprehensive social media management. Our expert team leverages the power of social platforms to build your brand, engage your audience, and drive conversions. Our services include:</h1>
+            <h1 className='font-poppins md:text-2xl text-lg my-16 font-medium text-white md:text-center w-[80vw]'><span className='text-[#FC7901]'>At MarketingEXL,</span> {service.description}</h1>
 
-            <div className='w-screen flex justify-center items-center gap-6'>
-                <div className='w-1/2 flex justify-center items-center text-white'>
-                    <Accordion type="single" collapsible className="w-[80%]">
-                        <AccordionItem value="item-1">
-                            <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                            <AccordionContent>
-                            Yes. It adheres to the WAI-ARIA design pattern.
-                            </AccordionContent>
-                        </AccordionItem>
-                        <AccordionItem value="item-2">
-                            <AccordionTrigger>Is it styled?</AccordionTrigger>
-                            <AccordionContent>
-                            Yes. It comes with default styles that matches the other
-                            components&apos; aesthetic.
-                            </AccordionContent>
-                        </AccordionItem>
-                        <AccordionItem value="item-3">
-                            <AccordionTrigger>Is it animated?</AccordionTrigger>
-                            <AccordionContent>
-                            Yes. It's animated by default, but you can disable it if you prefer.
-                            </AccordionContent>
-                        </AccordionItem>
+            <div className='w-screen md:flex justify-center items-center gap-6'>
+                <div className='md:w-1/2 flex justify-center items-center text-white'>
+                    <Accordion type="single" collapsible className="w-[85%]">
+                        {
+                            service.accordionTitleArr.map((title, index) => (
+                                <AccordionItem key={index} value={`item-${index}`} className="border-2 bg-cover border-[#FC7901] rounded-3xl px-4 py-2 mb-4 bg-no-repeat" style={{ backgroundImage: 'url(/images/cards/Card.png)' }}>
+                                    <AccordionTrigger>{title}</AccordionTrigger>
+                                    <AccordionContent>
+                                        {service.accordionDescArr[index]}
+                                    </AccordionContent>
+                                </AccordionItem>
+                            ))
+                        }
                     </Accordion>
                 </div> 
-                <div className='w-1/2 text-white'>
-                    <img src={service.imgSrc} alt="" />
+                <div className='md:w-1/2 text-white flex justify-center items-center'>
+                    <img src={service.photoUrl} alt="" className='md:w-[80%] rounded-3xl hidden md:block'/>
                 </div>
             </div>
 
