26c776e1937de335805e6548418fce81bd038f10
Prince Mishra <pm618209@gmail.com>
2025-04-08 12:57:09 +0530

    blog page dynamic and add google sheet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
-import { Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
+import { Route, Routes } from "react-router-dom"
 import { useEffect, useState } from "react";
 import { addBlogsItems } from "./redux/slices/BlogSlice"; // Adjust the path as per your project structure
 
-=======
->>>>>>> 3a7e0ecca5f07246720cbfb7969eb4d641ebf7be
+
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Navbar from "./components/Navbar";
@@ -15,16 +13,11 @@ import ScrollToTop from "./components/ScrollToTop";
 import ContactUs from "./pages/Contact/ContactUs";
 import ServiceDetail from "./pages/Services/components/ServiceDetail";
 import PageTransition from "./components/PageTransition";
-<<<<<<< HEAD
-=======
-import { useState } from "react";
->>>>>>> 3a7e0ecca5f07246720cbfb7969eb4d641ebf7be
 import FloatButton from "./components/FloatButton";
 import FormPopup from "./components/FormPopup";
 import Blog from "./pages/Blog/Blog";
 import BlogDetails from "./pages/Blog/BlogDetails";
 import ErrorPage from "./pages/ErrorPage";
-<<<<<<< HEAD
 import { useAppDispatch } from "./redux/hook";
 
 function App() {
@@ -32,13 +25,6 @@ function App() {
   
   const [showForm, setShowForm] = useState(false);
   
-=======
-import { blogItems } from "./utils/Constant";
-
-function App() {
-  const [showForm, setShowForm] = useState(true); // Show the form by default
-
->>>>>>> 3a7e0ecca5f07246720cbfb7969eb4d641ebf7be
   const toggleForm = () => {
     setShowForm(false); // Close the form when the user submits or closes it
   };
@@ -55,7 +41,6 @@ function App() {
 
 
 
-<<<<<<< HEAD
 
   return (
     <>
@@ -81,43 +66,6 @@ function App() {
 
         <Footer />
       </div>
-=======
-  return (
-    <>
-      {showForm ? (
-        <FormPopup onClose={toggleForm} /> // Show the form popup
-      ) : (
-        <>
-          <Navbar formVis={() => setShowForm(true)} />
-          <div className="max-w-full overflow-hidden bg-black">
-            <FloatButton />
-            <ScrollToTop />
-
-            <PageTransition>
-              <Routes>
-                <Route path="/" element={<Home toggleForm={() => setShowForm(true)} />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/services" element={<Services />} />
-                <Route
-                  path="/packages"
-                  element={<Package toggleForm={() => setShowForm(true)} />}
-                />
-                <Route path="/contact" element={<ContactUs />} />
-                <Route path="/blog" element={<Blog blogItems={blogItems} />} />
-                <Route
-                  path="/blog/:id"
-                  element={<BlogDetails blogItems={blogItems} />}
-                />
-                <Route path="/service/:id" element={<ServiceDetail />} />
-                <Route path="*" element={<ErrorPage />} />
-              </Routes>
-            </PageTransition>
-
-            <Footer />
-          </div>
-        </>
-      )}
->>>>>>> 3a7e0ecca5f07246720cbfb7969eb4d641ebf7be
     </>
   );
 }
